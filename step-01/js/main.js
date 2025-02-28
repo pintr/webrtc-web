46a381b9ff4081344a7c4da4aaffb6faa9c275c4
pintr <michele.brigadoi@unibo.it>
2025-02-28 18:45:07 +0100

    Stream video with RTCPeerConnection.

--- step-01/js/main.js
+++ step-01/js/main.js
@@ -1,27 +1,46 @@
 'use strict';
 
-// On this codelab, you will be streaming only video (video: true).
-const mediaStreamConstraints = {
-  video: true,
-};
+// const used as an argument to getUserMedia, specifying what media to get, could have had "audio" too
+// The constraints concern other paramteres too, such as widht, height, and frame rate for videos.
+const mediaStreamingConstraints = {
+  audio: false,
+  // video: true,
+  video: {
+    width: {
+      min: 640,
+      max: 1920
+    },
+    height: {
+      max: 1080
+    },
+    frameRate: {
+      min: 3,
+      max: 60
+    }
+  }
+}
 
-// Video element where stream will be placed.
-const localVideo = document.querySelector('video');
+// Select video element in the HTML page where the stream will be placed.
+const localVideo = document.querySelector('video')
 
-// Local stream that will be reproduced on the video.
+// Variable containing the media stream for debugging
 let localStream;
 
-// Handles success by adding the MediaStream to the video element.
-function gotLocalMediaStream(mediaStream) {
+// Function that handles the webcam media stream if the browser can access it succesfully.
+// It set the stream to the video element, and to the localStream variable
+function gotLocalVideoStream(mediaStream) {
   localStream = mediaStream;
   localVideo.srcObject = mediaStream;
 }
 
-// Handles error by logging a message to the console with the error message.
+// Function that handles the arror given by the media stream by printing it
 function handleLocalMediaStreamError(error) {
-  console.log('navigator.getUserMedia error: ', error);
-}
+  console.log('navigator.mediaDevices.getUserMedia error: ', error);
+};
 
-// Initializes media stream.
-navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-  .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+// Initialise the video stream.
+// The navigator.mediaDevices.getUserMedia function lets the browser request permission from user to access the camera.
+// If successful the stream is returned to gotLocalVideoStream that assigns it to the video element.
+// Otherwise an error is caught by the handleLocalMediaStreamError function, that prints it.
+navigator.mediaDevices.getUserMedia(mediaStreamingConstraints)
+  .then(gotLocalVideoStream).catch(handleLocalMediaStreamError)
